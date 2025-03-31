7b91978209b980253e73b4082b1804d7d019c832
Tim Schulz <tim.schulz.rc@gmail.com>
2025-03-31 12:38:45 +0200

    make header force dynamic

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,7 @@
 import { createClient } from "@/prismicio";
 import Navigation from "./navigation";
 
-export const revalidate = 0;
+export const dynamic = "force-dynamic";
 
 export default async function Header() {
   const client = createClient();
